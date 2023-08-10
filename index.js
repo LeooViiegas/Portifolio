0327aa2b7c110368d11e0eada108ed6425e9e21d
Leonardo Oliveira <leonardo.viegaso@gmail.com>
2023-08-10 11:19:00 -0300

    Nova UI

---
+++ index.js
@@ -0,0 +1,25 @@
+const textElement = document.getElementById('text');
+const text = 'Olá! Eu sou <span class="destaque">Leonardo Oliveira</span>, desenvolvedor Front-End.';
+let index = 0;
+
+function typeText() {
+	if (index <= text.length) {
+	  textElement.innerHTML = text.slice(0, index);
+	  index++;
+	}
+	
+	if (index > text.length) {
+		setTimeout(() => {
+		  textElement.innerHTML = text;
+		  setTimeout(() => {
+			textElement.innerHTML = '';
+			index = 0;
+			setTimeout(typeText, 60);
+		  }, 3000);
+		}, 60);
+	  } else {
+		setTimeout(typeText, 60);
+	  }
+	}
+	
+	typeText();
